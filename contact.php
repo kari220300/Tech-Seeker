c103a8c15f6c034734f67b78fa77707c844ee61c
unknown <karishma.potdukhe@gmail.com>
2021-02-15 10:53:13 +0530

    First Commit

---
+++ contact.php
@@ -0,0 +1,26 @@
+<!--Start Contact Us-->
+<div class="container mt-4" id="Contact">  <!--Start Contact Us Container-->
+      <h2 class="text-center mb-4">Contact Us</h2> <!-- Contact Us Heading -->
+      <div class="row">  <!--Start Contact Us Row-->
+        <div class="col-md-8"> <!--Start Contact Us 1st Column-->
+          <form action="" method="post">
+            <input type="text" class="form-control" name="name" placeholder="Name"><br>
+            <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+            <input type="email" class="form-control" name="email" placeholder="E-mail"><br>
+            <textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
+            <input class="btn btn-info" type="submit" value="Send" name="submit"><br><br>
+          </form>
+        </div> <!-- End Contact Us 1st Column-->
+
+        <div class="col-md-4 stripe text-white text-center" style=" background: linear-gradient(to right, #0575e6, #021b79);">  <!-- Start Contact Us 2nd Column-->
+          <h4>Tech Seeker</h4>
+          <p>LT Road, 
+          Near RK Restaurant,
+          Borivali West 
+          Mumbai - 400092<br /> 
+          Phone: +1234567890 <br />
+          www.elearn.com </p>
+        </div> <!-- End Contact Us 2nd Column-->
+      </div> <!-- End Contact Us Row-->
+    </div> <!-- End Contact Us Container-->
+    <!-- End Contact Us -->
